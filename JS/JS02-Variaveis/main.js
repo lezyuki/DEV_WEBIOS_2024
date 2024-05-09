f65fe612abc89330802b292c058493047c248620
lezyuki <leluiiz.2019@gmail.com>
2024-05-08 21:42:57 -0300

    aula2 js

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,42 @@
+console.log("Hello World!")
+
+// variaveis 
+//Quero Criar um espaço na memoria do computador chamado _cleber e dizer que esse espaço n memoria irá receber o valor 8
+
+var _cleber = 8
+_cleber = "Messi"
+
+console.log(_cleber)    // redeclarar variavel
+
+let letsgo = 4  // com let não posso redeclarar
+letsgo = 6 
+
+console.log(letsgo)
+
+console.clear()
+
+const PI = 92392 
+
+console.log(PI)
+
+ // com const não posso redeclarar e alterar o valor
+
+ let NAN = 0 / 0
+ console.log(NAN)
+
+ let ios = "Instituto da oportunidade social" 
+
+ console.log("Instituto" + "da" + "oportunidade" + "social")
+
+ var numero1 = 2
+ var texto = "Eu tenho"
+ var texto2 = " Reais "
+
+ console.log(texto + numero1 + texto2) // concatenar = é possivel concatenar Strings ou concatenar variaveis
+
+ var num1 = 1 
+ var num2 = 5
+ // num1 é igual a num2 
+ // 1 é igual a 1 = true
+
+ console.log(num1 == num2)
