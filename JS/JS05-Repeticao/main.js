931fa3e5f06e9e7c494d47bee22b9a44367d37fd
lezyuki <leluiiz.2019@gmail.com>
2024-05-15 21:29:09 -0300

    js05

---
+++ JS/JS05-Repeticao/main.js
@@ -0,0 +1,18 @@
+// while 
+// enquanto 0 < 10 vou apresentar a mensagem - 0 ainda é menor que 10 -
+//while(contador <= 10) {
+  // console.log(contador + " ainda é menor que 10")
+   //debugger
+   //contador++
+   // contador = 0 + 1
+   // contador = 1
+// }
+ 
+var contador = 0
+ 
+do{
+  debugger
+  console.log(contador + " ainda é menor que 10 ")
+
+  contador++
+} while (contador < 10)
